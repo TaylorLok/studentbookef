35af19c8c173e83d636cf548dc1b1215be8bb582
TaylorLok <lokombetaylor1223@gmail.com>
2020-04-17 17:54:03 +0200

    created 3 classes and their Io

---
+++ io/location_io.go
@@ -0,0 +1,60 @@
+package io
+
+import (
+	"errors"
+	"studentbookef/api"
+	"studentbookef/domain"
+)
+
+const locationURL = api.BASE_URL + "loaction/"
+
+func CreateLocation(location domain.Location)(domain.Location,error)  {
+	entity:= domain.Location{}
+	resp,_ := api.Rest().SetBody(location).Post(locationURL + "create")
+	if resp.IsError() {
+		return entity, errors.New(resp.Status())
+	}
+	err := api.JSON.Unmarshal(resp.Body(), &entity)
+	if err != nil {
+		return entity, errors.New(resp.Status())
+	}
+	return entity, nil
+}
+func ReadLocation(id string)(domain.Location,error)  {
+	entity := domain.Location{}
+	resp, _ := api.Rest().Get(locationURL + "read?id=" + id)
+	if resp.IsError() {
+		return entity, errors.New(resp.Status())
+	}
+	err := api.JSON.Unmarshal(resp.Body(), &entity)
+	if err != nil {
+		return entity, errors.New(resp.Status())
+	}
+	return entity, nil
+}
+func UpdateLocation(location domain.Location)(domain.Location,error){
+	entity := domain.Location{}
+	resp, _ := api.Rest().SetBody(location).Post(locationURL + "update")
+	if resp.IsError() {
+		return entity, errors.New(resp.Status())
+	}
+	err := api.JSON.Unmarshal(resp.Body(), &entity)
+	if err != nil {
+		return entity, errors.New(resp.Status())
+	}
+	return entity, nil
+}
+func DeleteLocation(location domain.Location)(domain.Location,error)  {
+	entity := domain.Location{}
+	resp, _ := api.Rest().SetBody(location).Post(locationURL + "delete")
+	if resp.IsError() {
+		return entity, errors.New(resp.Status())
+	}
+	err := api.JSON.Unmarshal(resp.Body(), &entity)
+	if err != nil {
+		return entity, errors.New(resp.Status())
+	}
+	return entity, nil
+}
+
+
